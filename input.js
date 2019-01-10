79e77ec822f4a3de30ab9b2ce99a22b02c30dbbb
zura1547 <zura.benashvili@gmail.com>
2019-01-10 11:25:09 +0400

    chrome on windows now supports input[type="date"]

--- input.js
+++ input.js
@@ -136,14 +136,6 @@ export default class Input {
 
 	// Return false if the browser does not support input[type="date"].
 	static supportsDateInput() {
-
-		var isChrome = /Chrome/.test(navigator.userAgent) && /Google Inc/.test(navigator.vendor);
-		var isWindows=(navigator.userAgent.toLowerCase().indexOf("windows") > -1);
-
-		if(isChrome && isWindows){
-			return false
-		}
-
 		const input = document.createElement(`input`);
 		input.setAttribute(`type`, `date`);
 
